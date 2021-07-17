0c854066f5b0ea5acae29025d82168ed4cd6a3a6
IsmaelRodriguezHanco <70766663@ucsm.edu.pe>
2021-07-16 23:19:45 -0500

    actualizacion de las clases Usuario para usar hashing, e implementacion de clase register y hash para trbajar con hashing

---
+++ Proyecto3raFase/src/Persona.java
@@ -0,0 +1,48 @@
+
+public class Persona{
+	
+	private String nombre;
+	private String apePaterno;
+	private String apeMaterno;
+	private int DNI;
+	
+	public Persona(){
+		this.nombre = "";
+		this.apePaterno = "";
+		this.apeMaterno = "";
+		this.DNI = 0;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApePaterno() {
+		return apePaterno;
+	}
+	public void setApePaterno(String apePaterno) {
+		this.apePaterno = apePaterno;
+	}
+	public String getApeMaterno() {
+		return apeMaterno;
+	}
+	public void setApeMaterno(String apeMaterno) {
+		this.apeMaterno = apeMaterno;
+	}
+	public int getDNI() {
+		return DNI;
+	}
+	public void setDNI(int dni) {
+		this.DNI = dni;
+	}
+	@Override
+	public String toString() {
+		return "Datos Personales Ingresados:\n"
+				+ "Nombre: " + this.nombre
+				+ "\nApellido Paterno: " + this.apePaterno
+				+ "\nApellido Materno: " + this.apeMaterno
+				+ "\nDNI: "+ this.DNI + "\n";
+	}
+
+}
